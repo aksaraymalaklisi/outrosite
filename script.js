58b85c4db4dddb07a134cc0bef8ecc7f3098af5e
aksaraymalaklisi <aksaraymalaklisi@protonmail.com>
2024-06-07 17:56:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function revelar() {
+    document.getElementById("interativo1").innerHTML = "Essa imagem não é minha. O razão é óbvia: eu estou desempregado.";
+}
+
+  function revelar2() {
+    document.getElementById("interativo2").innerHTML = "Essa imagem está má recortada.";
+}
+
+function revelar3() {
+    document.getElementById("interativo3").innerHTML = "Também não é eu. O rosto é completamente diferente.";
+}
